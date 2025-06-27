3f393a02fdcc03f752b788557b248e54f4b955d5
YUSIFABOOD <yousefabood84@gmail.com>
2025-06-27 21:44:00 +0300

    Cmake done

---
+++ Cmake and make/CMakeFiles/text.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/yusif/CS2 assignment/main.cpp" "CMakeFiles/text.dir/main.cpp.o" "gcc" "CMakeFiles/text.dir/main.cpp.o.d"
+  "/home/yusif/CS2 assignment/text_utility.cpp" "CMakeFiles/text.dir/text_utility.cpp.o" "gcc" "CMakeFiles/text.dir/text_utility.cpp.o.d"
+  "" "text" "gcc" "CMakeFiles/text.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cmake and make/CMakeFiles/text.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text.dir/link.d"
+  "CMakeFiles/text.dir/main.cpp.o"
+  "CMakeFiles/text.dir/main.cpp.o.d"
+  "CMakeFiles/text.dir/text_utility.cpp.o"
+  "CMakeFiles/text.dir/text_utility.cpp.o.d"
+  "text"
+  "text.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
