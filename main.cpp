7391e0b5a5ecf009e8fefacb4be7e5fc2b1f768d
YUSIFABOOD <yousefabood84@gmail.com>
2025-06-27 16:06:48 +0300

    updates

--- main.cpp
+++ main.cpp
@@ -3,45 +3,46 @@
 using namespace std;
 
 int main () {
-    cout<<"Is the word hello a palindrome?"<<endl;
+    cout<<"Is the word (hello) a palindrome?"<<endl;
 
     if(isPalindrom("hello")) cout<<"It is a palindrome."<<endl;
     else cout<<"It is not a palindrome."<<endl;
 
-    cout<<"Is the word helloolleh a palindrome?"<<endl;
+    cout<<endl<<"Is the word (helloolleh) a palindrome?"<<endl;
 
     if(isPalindrom("helloolleh")) cout<<"It is a palindrome."<<endl;
     else cout<<"It is not a palindrome."<<endl;
 
-    cout<<"In the follwing two funcs, we change all characters of the sentence"<<endl<<" goOd MorniNG to uppercase then to lowercase."<<endl;
-    char str [1000] = "goOd MorniNG";
+    cout<<endl<<"Changing all characters of (goOd MorniNG) to uppercase"<<endl;
+    char str [] = "goOd MorniNG";
     Uppercase(str);
     cout <<str<<endl;
 
-    char str2 [1000] = "goOd MorniNG";
+    cout<<endl<<"Changing all characters of (goOd MorniNG) to lowercase"<<endl;
+    char str2 [] = "goOd MorniNG";
     Lowercase(str2);
     cout <<str2<<endl;
 
-    cout<<"An example for the substring occurence counter."<<endl<<"We count the occurence of the substring AU in the sentence AUC."<<endl;
+    cout<<endl<<"Counting the occurence of the substring (AU) in (AUCgoAUCnoAUCyes AUC)."<<endl;
 
     int count = substringoccur("AUCgoAUCnoAUCyes AUC", "AU");
-    cout<<count<<endl;
+    cout<<"Number of occurence: "<<count<<endl;
 
-    cout<<"Counting the vowels and the consonants in the string Fundamentals of computing: "<<endl;
+    cout<<endl<<"Counting the vowels and the consonants in (Fundamentals of computing): "<<endl;
 
     countVowelsAndConsonants("Fundamentals of computing");
 
-    cout<<"Testing reverse string func with string Malak:"<<endl;
+    cout<<endl<<"Testing reverse string func on (Malak):"<<endl;
 
-    char str3 [1000] = "Malak";
+    char str3 [] = "Malak";
     reverseString(str3);
     cout<<str3<<endl;
 
-    cout<<"Counting the words in the sentece: You're gonna carry that weight."<<endl;
+    cout<<"Counting the words in (You're gonna carry that weight)."<<endl;
     int count2 = countW("You're gonna carry that weight.");
     cout<<"Number of words: "<<count2<<endl;
 
-    cout<<"Counting the characters in the previous sentence:"<<endl;
+    cout<<endl<<"Counting the characters in (You're gonna carry that weight):"<<endl;
 
     int count3 = countCharacters("You're gonna carry that weight.");
     cout<<"Number of characters: "<<count3<<endl;
